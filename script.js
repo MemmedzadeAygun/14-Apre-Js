bbea7e829e3430e5a686b56baed161c876ee3c08
MemmedzadeAygun <147386696+MemmedzadeAygun@users.noreply.github.com>
2025-04-21 08:10:33 -0700

    ok

---
+++ script.js
@@ -0,0 +1,49 @@
+
+// let username = "Nicat";
+// let username = "Eli";
+// username = "Aygun";
+// console.log(username);
+
+// var student = "Nigar";
+// var student = "Vali";
+// console.log(student);
+
+// const user = "Guler";
+// user = "Rustem";
+// const user = "Hemid";
+// console.log(user);
+
+// let num = 12;
+// let num;
+// let num = null;
+// console.log(num);
+
+// let num1 = 2;
+// let num2 = 3;
+
+// console.log(num1 + num2);
+// console.log(num1 - num2);
+// console.log(num1 * num2);
+// console.log(num1 / num2);
+// console.log(num1 % num2);
+// console.log(num1 ** num2);
+
+// let a = 12;
+// let b = 3;
+// a+=b
+// a = a+b;
+// console.log(a-=b);
+// a = a/b;
+// console.log(a/=b);
+// a = a*b;
+// console.log(a*=b);
+// console.log(a%=b);
+// console.log(a);
+// console.log(a**b);
+
+
+let bookPrice = 12;
+let bookAmount = 100;
+
+let result = bookPrice*bookAmount;
+console.log(result);
